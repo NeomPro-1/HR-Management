73871de3661bbd3d3daf3e59a572e4169415feb9
Maddi Harish <MADDIHARISHREDDY@gmail.com>
2025-10-08 15:00:52 +0000

    this is the error i got now : (External page)

--- src/lib/placeholder-data.ts
+++ src/lib/placeholder-data.ts
@@ -1,5 +1,8 @@
 
-import { PlaceHolderImages } from './placeholder-images';
+import imageData from './placeholder-images.json';
+import type { ImagePlaceholder } from './placeholder-images';
+
+const PlaceHolderImages: ImagePlaceholder[] = imageData.placeholderImages;
 
 const getImg = (id: string) => PlaceHolderImages.find(p => p.id === id)?.imageUrl || '';
 
