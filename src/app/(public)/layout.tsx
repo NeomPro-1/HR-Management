193bba80e608e68c7a6e563e6f7dedf146268ce0
Maddi Harish <MADDIHARISHREDDY@gmail.com>
2025-10-08 15:22:04 +0000

    remove the Paragraph of "The Source code is available on GitHub" in sign

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -32,7 +32,7 @@ export default function PublicLayout({
        <footer className="py-6 md:px-8 md:py-0 border-t">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
             <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by Synergy. The source code is available on GitHub.
+            Built by Synergy.
             </p>
         </div>
       </footer>
