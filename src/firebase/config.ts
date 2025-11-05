f666102dffff08cb1bf2e909ecc0819a3c9977eb
Your Name <MADDIHARISHREDDY@gmail.com>
2025-11-05 10:58:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4405265013-f6804",
+  "appId": "1:1084679414097:web:247e2455bff60e46df7370",
+  "apiKey": "AIzaSyBTfyApiJSHoX2yCB6qgJpyYXg5fD30dd4",
+  "authDomain": "studio-4405265013-f6804.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1084679414097"
+};
